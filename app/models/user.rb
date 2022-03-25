84f043cfa33913f7d1a93a9ee16056804e69c55c
NguyenRyo <montmori.chan@gmail.com>
2022-03-25 20:05:55 +0900

    バリデーション記載、ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname          , presence: true
+  validates :firstname_kanji   , presence: true
+  validates :lastname_kanji    , presence: true
+  validates :firstname_furigana, presence: true
+  validates :lastname_furigana , presence: true
+  validates :birthday          , presence: true
+
 end
