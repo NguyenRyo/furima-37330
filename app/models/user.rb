7a488114e5a0e25d12a9529d115d93f678b12a00
NguyenRyo <montmori.chan@gmail.com>
2022-03-31 15:18:43 +0900

    Userモデル単体テストコードの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze #氏名は全角でないと登録できない
   validates :nickname          , presence: true
-  validates :firstname_kanji   , presence: true
-  validates :lastname_kanji    , presence: true
+  validates :firstname_kanji   , presence: true, format: {with: VALID_NAME_REGEX, message: 'require full-width character'}
+  validates :lastname_kanji    , presence: true, format: {with: VALID_NAME_REGEX, message: 'require full-width character'}
   validates :firstname_furigana, presence: true
   validates :lastname_furigana , presence: true
   validates :birthday          , presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze #パスワードは英数混合でなければ保存できない
+  validates :password          , presence: true, format: {with: VALID_PASSWORD_REGEX, message: 'include both letters and numbers'}
 
 end
