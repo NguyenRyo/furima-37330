51934b7afed99a0ebf1b69097fd9b9a18a715bd5
NguyenRyo <montmori.chan@gmail.com>
2022-04-01 14:47:29 +0900

    名前カナ全角のバリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze #氏名は全角でないと登録できない
   validates :nickname          , presence: true
-  validates :firstname_kanji   , presence: true, format: {with: VALID_NAME_REGEX, message: 'require full-width character'}
-  validates :lastname_kanji    , presence: true, format: {with: VALID_NAME_REGEX, message: 'require full-width character'}
-  validates :firstname_furigana, presence: true
-  validates :lastname_furigana , presence: true
+
+  VALID_NAME_kanji_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze #氏名は全角でないと登録できない
+  validates :firstname_kanji   , presence: true, format: {with: VALID_NAME_kanji_REGEX, message: 'require full-width character'}
+  validates :lastname_kanji    , presence: true, format: {with: VALID_NAME_kanji_REGEX, message: 'require full-width character'}
+
+  VALID_NAME_furigana_REGEX = /\A[ァ-ヶー]+\z/.freeze #フリガナは全角でないと登録できない
+  validates :firstname_furigana, presence: true, format: {with: VALID_NAME_furigana_REGEX, message: 'require full-width Furigana character'}
+  validates :lastname_furigana , presence: true, format: {with: VALID_NAME_furigana_REGEX, message: 'require full-width Furigana character'}
+
   validates :birthday          , presence: true
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze #パスワードは英数混合でなければ保存できない
   validates :password          , presence: true, format: {with: VALID_PASSWORD_REGEX, message: 'include both letters and numbers'}
 
