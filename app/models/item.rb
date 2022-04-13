f823c0663b183eab65928b0fc8ef2773b4bcba32
NguyenRyo <montmori.chan@gmail.com>
2022-04-13 11:19:00 +0900

    Itemモデルのバリデーション修正
    手数料計算はコメントアウトし一時保留中

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category, :condition, :deliver_fee_cover, :prefecture, :deliver_date
 
   #空の投稿を保存できないようにする
-  validates :title, :text, presence: true
+  validates :title, :description, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id,           numericality: { other_than: 1 , message: "can't be blank"}
@@ -11,5 +11,15 @@ class Item < ApplicationRecord
   validates :prefecture_id,         numericality: { other_than: 0 , message: "can't be blank"}
   validates :deliver_fee_cover_id,  numericality: { other_than: 1 , message: "can't be blank"}
   validates :deliver_date_id,       numericality: { other_than: 1 , message: "can't be blank"}
-    
+  
+  # VALID_price_REGEX = [0-9].freeze #priceは半角でないと登録できない
+  # validates :price,                 numericality: { in: 300..9999999 , message: "can't be blank", with: VALID_price_REGEX}
+
+  # def tax_price
+  #   (self.price * 0.1).round
+  # end
+
+  # def profit
+  #   (self.price * 0.9).round
+  # end
 end
