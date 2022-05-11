bae24bc67f7816f259e83380e7a471d0d7cef035
NguyenRyo <montmori.chan@gmail.com>
2022-05-11 21:08:19 +0900

    Formオブジェクトを用いた複数のテーブルへの購入情報保存機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,11 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: :index
 
   def index
     @purchase_buyer = PurchaseBuyer.new
     @item = Item.find(params[:item_id])
   end 
-
-  def new
-  end
   
   def create
     @purchase_buyer = PurchaseBuyer.new(purchase_params)
@@ -15,7 +13,8 @@ class PurchasesController < ApplicationController
       @purchase_buyer.save
       redirect_to root_path
     else
-      render :new
+      @item = Item.find(params[:item_id])
+      render :index
     end
   end
 
@@ -24,4 +23,12 @@ class PurchasesController < ApplicationController
   def purchase_params
     params.require(:purchase_buyer).permit(:postalcode, :prefecture_id, :city, :address, :building, :tel).merge(item_id: params[:item_id], user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+  
 end
