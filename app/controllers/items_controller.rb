afae80f0f182e67a5872f3f210da19f27b9b406b
NguyenRyo <montmori.chan@gmail.com>
2022-05-20 20:25:03 +0900

    売り切れ時の商品一覧への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create, :edit, :destroy]
     before_action :item_find, only:[:show, :edit, :update, :destroy]
     before_action :move_to_index, only: [:edit, :destroy]
+    before_action :move_to_index_soldout, only: [:edit]
 
   def index
     @items = Item.all
@@ -53,7 +54,12 @@ class ItemsController < ApplicationController
 
   def item_find
     @item = Item.find(params[:id])
- 
+  end
+
+  def move_to_index_soldout
+    if @item.purchase.present?
+      redirect_to action: :index
+    end
   end
 
 end
