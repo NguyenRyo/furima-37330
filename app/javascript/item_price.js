4cca857d5fb04750b4d14878800f456134f69f89
NguyenRyo <montmori.chan@gmail.com>
2022-05-26 21:34:53 +0900

    S3実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
   const itemPrice = document.getElementById("item-price");
 
   itemPrice.addEventListener("input", () => {
-    const itemPriceValue = itemPrice.value
+    const itemPriceValue = itemPrice.value;
     const Tax = 0.1;
     const tax_fee = Math.floor(itemPriceValue * Tax);
     const profit = Math.floor(itemPriceValue - tax_fee);
